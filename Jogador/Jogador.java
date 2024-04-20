9cb745e3712996da1a4693543a04cb50a08d055c
Messias-Acacy <130923301+Messias-Acacy@users.noreply.github.com>
2024-04-19 23:27:14 -0300

    Add files via upload

--- Jogador/Jogador.java
+++ Jogador/Jogador.java
@@ -2,21 +2,87 @@ package Jogador;
 
 
 import java.util.Scanner;
-@SuppressWarnings("unused")
+import Tabuleiro.Campo;
 public class Jogador {
 	private String nome;
 	private String controlePeca;
-	private boolean isMeuTurno;
 	private boolean isRookieFeito;
+	private Scanner sr;
 
 	
 	
 	
-	public Jogador(String nome, String controlePeca) {
-		this.nome = nome;
-		this.setControlePeca(controlePeca);
-		this.isMeuTurno = false;
+	public Jogador(String nome) {
+		this.nome = nome.toLowerCase();
+		this.sr =  new Scanner(System.in);
+
 		this.isRookieFeito = false;
+	}
+	
+	
+public int[] SelecionarPeca() {
+		int posicaoX = -1;
+		int posicaoY = -1;
+		System.out.println();
+		boolean isPrimeiroLoop = true;
+		String getPosicao ="";
+		
+		do {
+			if(!isPrimeiroLoop) {
+				System.out.println("Você digitou uma posição inválida! digite outra vez: ");
+				System.out.print("Digite a posicao[numberLine]: ");
+				getPosicao = sr.next();
+				
+			}
+			else {
+				System.out.print("Digite a posicao[numberLine]: ");
+				getPosicao = sr.next();
+			}
+			
+		
+		
+		posicaoX = Integer.parseInt(getPosicao.substring(0,1))-1;
+		String letra = getPosicao.substring(1).toLowerCase();
+		
+		String[]letras = {"a","b","c","d","e","f","g","h"};
+		for (int i = 0; i < letras.length; i++) {
+			if(letras[i].equals(letra)) {
+				posicaoY = i;
+				break;
+			}
+		}
+		if(isPrimeiroLoop) {
+			isPrimeiroLoop = false;
+		}
+		} while ((posicaoY < 0 || posicaoY >= 8) || (posicaoX < 0 || posicaoX >= 8));
+		
+		
+		
+		return new int[] {posicaoX,posicaoY};
+	
+	}
+	
+	
+public void MovimentarPeca(Campo tabuleiro, int[] pos) {
+	
+	
+	int[] posicoes =pos;
+	
+	while (tabuleiro.getPecas()[posicoes[0]][posicoes[1]] == null) {
+		System.out.print("Você selecionou uma posição vazia! Digite outra vez: ");
+		posicoes =this.SelecionarPeca();
+
+}
+		
+		//int[] retorno = this.getTabuleiro().getPecas()[posicoes[0]][posicoes[1]].movimentar(posicoes[0],posicoes[1]);
+		//imitando o retorno
+		
+	int[] retorno = this.SelecionarPeca();
+	tabuleiro.getPecas()[retorno[0]][retorno[1]] =tabuleiro.getPecas()[posicoes[0]][posicoes[1]];
+	tabuleiro.getPecas()[posicoes[0]][posicoes[1]] = null;
+	tabuleiro.getPecas()[retorno[0]][retorno[1]].setPosicao(new int[] {retorno[0],retorno[1]});
+
+	
 	}
 	
 
@@ -39,16 +105,6 @@ public class Jogador {
 
 
 
-
-	public boolean isMeuTurno() {
-		return isMeuTurno;
-	}
-
-
-
-	public void setMeuTurno(boolean isMeuTurno) {
-		this.isMeuTurno = isMeuTurno;
-	}
 
 
 
--- Jogador/MainJogador.java
+++ Jogador/MainJogador.java
@@ -2,7 +2,7 @@ package Jogador;
 
 public class MainJogador {
 	public static void main(String[] args) {
-		Jogador j = new Jogador("Messias","branco");
+		Jogador j = new Jogador("Messias");
 		j.getControlePeca();
 
 	}
